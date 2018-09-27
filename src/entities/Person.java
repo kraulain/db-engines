db3c660be89ad9406c2b1acf80b49c0055e853f1
Fosso Arcel <kraulain@gmail.com>
2018-09-27 11:23:57 +0100

    add a person entitiy

---
+++ src/entities/Person.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class Person {
+    private String id;
+    private String name;
+    private int age;
+
+    public Person() {    }
+
+    public Person(String name, int age) {
+        this.setName(name);
+        this.setAge(age);
+    }
+
+    public Person(String id, String name, int age) {
+        this.setId(id);
+        this.setName(name);
+        this.setAge(age);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + getName() + ", Age: " + getAge();
+    }
+}
